3be9f512be443df2d2d32417d2ad91c8684b3cb2
Lol Mettam <49613065+sideshowlol@users.noreply.github.com>
2020-04-29 19:04:29 +0100

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,7 +10,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/hero2')`,
+            backgroundImage: `url('/img/hero2.jpg')`,
           }}
         >
           <h1
